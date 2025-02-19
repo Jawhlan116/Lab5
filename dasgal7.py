3fe9807519d8c53f689819ca3af8f8be563e82c9
Jawhlan116 <jawkhlan2005@gmail.com>
2025-02-19 12:02:51 +0800

    Hurdnii medeelel

---
+++ dasgal7.py
@@ -0,0 +1,11 @@
+a=float(input())
+if(a<11):
+    print("удаан")
+elif(a<51):
+    print("дундаж")
+elif(a<151):
+    print("хурдан")
+elif(a<1001):
+    print("их хурдан")
+else:
+    print("хэт хурдан")
